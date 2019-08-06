28caf6bedb8c29fd5776e96c244c7c88af78640d
wladyslaw-malejew <wladyslaw.malejew@gmail.com>
2019-08-06 12:01:27 +0300

    base done

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+      <input type="file" name="myFile">
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style>
+body {
+  height: 90vh;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
